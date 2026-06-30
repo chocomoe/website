3fa49f126f51f4a1b36c5591c9e8d9100c9731de
chocomoe <info.chocomoe@gmail.com>
2026-06-29 21:32:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const button = document.querySelector(".btn");
+
+    if (button) {
+        button.addEventListener("click", () => {
+
+            alert("Bienvenue chez ChocoMoe 🍫");
+
+        });
+    }
+
+});
